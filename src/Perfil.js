e347ff697c7b13e15b63fef777d52a97c7a267dd
Sergio-Mellado <107446622+Sergio-Mellado@users.noreply.github.com>
2023-11-16 16:24:42 -0300

    arreglos y mejora estetica

--- src/Perfil.js
+++ src/Perfil.js
@@ -9,7 +9,7 @@ const PerfilUsuario = () => {
     name: 'pancho',
     email: 'uwupancho@duocuc.cl.com',
     informacion: 'Mi nombre es Francisco Arias y soy productor de manzanas rojas, las cuales son las más jugosas.',
-    profilePicture: '../src/Logo/Feria virtual.png', 
+    profilePicture: '/Logo/Feria virtual.png', // Ruta ajustada para la imagen
   });
 
   const [mostrarFormularioSolicitud, setMostrarFormularioSolicitud] = useState(false);
@@ -25,33 +25,30 @@ const PerfilUsuario = () => {
   const handleInformacionChange = (e) => {
     setUser({ ...user, informacion: e.target.value });
   };
-  
-
-  const handleProfilePictureChange = (e) => {
-    setUser({ ...user, profilePicture: URL.createObjectURL(e.target.files[0]) });
-  };
 
   const handleLogout = () => {
-    navigate('/login'); // Redirige al usuario de vuelta a la página de inicio de sesión
+    navigate('/login');
   };
 
   const handleSolicitudes = () => {
-    // Mostrar u ocultar el formulario al presionar el botón "Solicitudes"
     setMostrarFormularioSolicitud(!mostrarFormularioSolicitud);
   };
 
   const handleProductos = () => {
-    navigate('/productos'); // Redirige al usuario a la página de productos
+    navigate('/productos');
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    // Lógica para manejar el envío del formulario
+    console.log('Formulario enviado:', user);
   };
 
   return (
     <div className="perfil-container">
       <div className="perfil-info">
-        <label>
-          <input type="file" accept="image/*" onChange={handleProfilePictureChange} />
-        </label>
-        {user.profilePicture && <img src={user.profilePicture} alt="Editar Foto" />}
-        <div>
+        {user.profilePicture && <img src={user.profilePicture} alt="Foto de perfil" />}
+        <div className="user-container">
           <h2>{user.name}</h2>
           <p>{user.email}</p>
           <p>{user.informacion}</p>
@@ -62,53 +59,45 @@ const PerfilUsuario = () => {
         <button onClick={handleProductos}>Productos</button>
         <button onClick={handleLogout}>Salir</button>
       </div>
-{mostrarFormularioSolicitud && (
-  <div className="formulario-solicitud">
-    <form>
-      <h3>Realizar Solicitud</h3>
-      <p></p>
-      <div>
-        <label>N° Solicitud</label>
-        {/* Aquí puedes manejar la lógica para generar el número de solicitud */}
-        <input type="text" placeholder="Autoincremental" disabled />
-      </div>
-      <div>
-      <p></p>
-        <label>Tipo de Producto </label>
-        <select>
-          <p></p>
-          <option value="frutas">Frutas</option>
-          <p></p>
-          <option value="verduras">Verduras</option>
-        </select>
-        <p></p>
-        <p></p>
-      </div>
-      <div>
-        <label>Transportista </label>
-        <select>
-          <option value="donPepe">Transporte Don Pepe</option>
-          <p></p>
-          <option value="limitada">Transporte Limitada</option>
-          <p></p>
-          <option value="valdes">Transportes Valdes</option>
-          
-          <option value="sayago">Transportes Sayago</option>
-        </select>
-          <div>
-          <label>Cantidad </label>
-          </div>
-      </div>
-      <div className="botones">
-      <p></p>
-      <p></p>
-        <button type="submit">Solicitar</button>
-        <p></p>
-        <button type="button">Ver Estado</button>
-      </div>
-    </form>
-  </div>
-)}
+
+      {mostrarFormularioSolicitud && (
+        <div className="formulario-solicitud">
+          <form onSubmit={handleFormSubmit}>
+            <h3>Realizar Solicitud</h3>
+            <div>
+              <label>N° Solicitud</label>
+              <input type="text" placeholder="Autoincremental" disabled />
+            </div>
+            <div>
+              <label>Seleccionar tipo</label>
+              <select>
+                <option value="frutas">Frutas</option>
+                <option value="verduras">Verduras</option>
+              </select>
+            </div>
+            <br></br>
+            <div>
+              <label>Transportista</label>
+              <select>
+                <option value="donPepe">Transporte Don Pepe</option>
+                <option value="limitada">Transporte Limitada</option>
+                <option value="valdes">Transportes Valdes</option>
+                <option value="sayago">Transportes Sayago</option>
+              </select>
+            </div>
+            <br></br>
+            <div>
+              <label>Cantidad (Kg)</label>
+              <input type="number" />
+            </div>
+            <br></br>
+            <div className="botones">
+              <button type="button" name="botton-solicitar">Solicitar</button>
+              <button type="button" name="botton-Verestado">Ver Estado</button>
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
